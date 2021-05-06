1617ef8833197ea9a38f4562d4e69bb096190bfd
Axel FRANCOIS-CHARLOT <axel.francois-charlot@cpe.fr>
2021-05-06 12:13:51 +0200

    Final

--- TP1/exo4.m
+++ TP1/exo4.m
@@ -5,16 +5,19 @@ clear variables;
 nue = 5000;
 [x,t,K] = SignalFonctionComposee(8,271,90*pi/180,5,1147,90*pi/180,-0.01012,0.01012,nue);
 
-
-plot(t,x)
+t = t*1000;
+stem(t,x)
+axis([t(1) t(end) min(x) max(x)])
 
 N= 512;
 
 zeroPadding = zeros(1, N-K);
-y = [x(floor(K/2)+1:K),zeroPadding, x(1:floor(K/2))];
+y = [x(floor(K/2)+1:K),zeroPadding, x(1:floor(K/2)+1)];
+figure(2)
+plot(0:1:length(y)-1,y)
 
 X = fft(y,N);
-figure(2);
+figure(3);
 nu = 0:nue/N:nue-nue/N;
 subplot(221)
 plot(nu,real(X))
